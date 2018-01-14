b326890ee21674d3bbd464c9b07b4192bfdb3334
Kaiwee <kaiweepuah0121@hotmail.com>
2018-01-14 19:24:46 +0800

    comment attr_reader

--- task.rb
+++ task.rb
@@ -1,6 +1,11 @@
 class Task
   attr_reader :description
 
+  # what attr_reader do is as below
+  # def description
+  #   @description
+  # end
+
   def initialize(description)
     @description = description
     @complete = false
